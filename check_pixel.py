2698c13f6c9317dc71438fc6decec972bb20e4d5
keiondesu96 <keiondesu96@gmail.com>
2019-05-01 10:07:44 +0300

    Initial version with summed magnitude values

---
+++ check_pixel.py
@@ -0,0 +1,19 @@
+import PIL as pil
+import math
+
+_MAGNITUDE = 15
+
+def find_approximate(pixel, pallette_list):
+    #don't change the alphas
+    if(pixel[3] <= 0):
+        return pixel
+
+    #current (ineffective) method: compute the sum and see which is less diff
+    pixel_sum = pixel[0]**_MAGNITUDE + pixel[1]**_MAGNITUDE + pixel[2]**_MAGNITUDE
+    pallette_pixel_sum = {}
+    for color in pallette_list:
+        i_sum = math.fabs(color[0]**_MAGNITUDE + color[1]**_MAGNITUDE + color[2]**_MAGNITUDE - pixel_sum)
+        pallette_pixel_sum[i_sum] = color
+
+    npixel = pallette_pixel_sum[min(pallette_pixel_sum.keys())]
+    return npixel
---
+++ main.py
@@ -0,0 +1,71 @@
+#python 2.7.13
+
+#-------------------------------------------------
+
+import sys, os, time
+if sys.version_info[0] > 2:
+  print("Python 2 only! Use PIL")
+  sys.exit(1)
+
+#--------------------------------------------------
+
+import check_pixel as chkpix
+import numpy
+import PIL as pil
+from PIL import Image
+
+#-------------------------------------------------
+
+print("Change the pallette of file to an approx from pallette.png")
+print("Works on every .png file in directory.")
+print("Press enter...")
+raw_input()
+
+print("\nsearching pallette file...")
+pallette_file = Image.open("pallette.png").convert('RGBA')
+width, height = pallette_file.size
+
+print("\nconstructing a pallette color list...")
+pallette_colors = []
+for x in range(width):
+    for y in range(height):
+        pixel = pallette_file.getpixel((x, y))
+        if pixel not in pallette_colors:
+            pallette_colors.append(pixel)
+            #print(pixel)
+
+#print("\nClose if you aren't okay with the pallette")
+#print("Enter to continue")
+#raw_input()
+
+
+print("\nloading source files..")
+default_dir = os.getcwd()
+os.chdir(default_dir + "\Source")
+source_files = {}
+for filename in os.listdir("."):
+    if filename.endswith('.png'):
+        source_files[filename] = Image.open(filename)
+
+
+print("\napproximating...\n")
+os.chdir(default_dir + "\Result")
+for file in source_files.keys():
+    print("file: " + file)
+    width, height = source_files[file].size
+
+    #create pixmap
+    pMap = source_files[file].load()
+
+    #run the script to change pixels to those of the pallette
+    for x in range(width):
+        for y in range(height):
+            pMap[x, y] = chkpix.find_approximate(pMap[x, y], pallette_colors)
+
+    source_files[file].save(file)
+    pass
+
+
+
+print("\ndone, press enter to exit")
+raw_input()
